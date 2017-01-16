e076c31bdeef98679e424ba298d52c81f737aeaf
Tim Downing <icedmetal57@gmail.com>
2017-01-16 11:31:14 -0600

    Project Setup

--- ProjectArcaneGrasp/Program.cs
+++ ProjectArcaneGrasp/Program.cs
@@ -14,7 +14,7 @@ namespace ProjectArcaneGrasp
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new ProjectArcaneGrasp())
                 game.Run();
         }
     }
